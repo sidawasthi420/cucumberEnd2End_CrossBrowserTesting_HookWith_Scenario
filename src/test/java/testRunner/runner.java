9311e8468c9a4b56556d0ee8cb9b5cc91156e84a
sidawasthi420 <144908193+sidawasthi420@users.noreply.github.com>
2025-10-30 00:41:51 +0530

    Refactor runner class to use TestNG with Cucumber

--- src/test/java/testRunner/runner.java
+++ src/test/java/testRunner/runner.java
@@ -1,11 +1,16 @@
 package testRunner;
 
-import org.junit.runner.RunWith;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.FeatureWrapper;
+import io.cucumber.testng.PickleWrapper;
+import io.cucumber.testng.TestNGCucumberRunner;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/resources/features/test.feature",
 		glue = {"stepDefinition","hooks"},
@@ -13,6 +18,29 @@ import io.cucumber.junit.CucumberOptions;
 		monochrome = true,   // to make console output more readable by removing unnecessary characters
 		tags = "@ChromeBrowser"
 		)
-public class runner {
+	
+public class runner extends AbstractTestNGCucumberTests{
+	
+	private TestNGCucumberRunner testNGCucumberRunner;
 
+
+    @BeforeClass( alwaysRun = true )
+    public void setUpClass()  {
+        testNGCucumberRunner = new TestNGCucumberRunner( this.getClass() );
+    }
+
+    @Test( groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features" )
+    public void feature( PickleWrapper pickleWrapper,FeatureWrapper cucumber Feature ) {
+        testNGCucumberRunner.runScenario( pickleWrapper.getPickle());
+    }
+
+    @DataProvider
+    public Object[][] features() {
+        return testNGCucumberRunner.provideScenarios();
+    }
+
+    @AfterClass( alwaysRun = true )
+    public void tearDownClass()  {
+        testNGCucumberRunner.finish();
+    }
 }
